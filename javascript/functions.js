f44a517e8301cc0b4e1ecaac4bf4a6ba33e2414e
George Nikiforidis <geonikif@hotmail.com>
2016-11-24 17:49:28 +0200

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,7 +4,43 @@ $(function() {
 
   getData();
 
+  // autoSearch();
+
+
+
 });
+
+
+// function autoSearch(){
+
+// // var key = API_KEY;
+
+// // var googleApiKey = "AIzaSyA-e9dYICVb1xm7zSM5uLEpUlYemugiMiA";
+
+
+
+// $("#search-inpt").autocomplete({
+
+//     source:function(){
+
+//         alert("yo");
+
+//     },
+
+//     select:function(){
+
+//     }
+
+
+
+
+// });
+
+// };
+
+
+
+
 
 
 function toggleMenu(){
@@ -18,7 +54,7 @@ function toggleMenu(){
 		
 	});
 
-}
+};
 
 
 function getData(){
@@ -35,7 +71,7 @@ $("#access").on("click", function(){
             // alert(myLoc);
 
         // var city = $("#search-inpt").val(); 
-         
+
         var apiCords= "http://api.openweathermap.org/data/2.5/weather?lat=" +myLoc[0]+ "&lon=" +myLoc[1]+ "&APPID=eaf69a1d8aef49e96abdc8a9b590ba64&units=metric";
 
           $.getJSON(apiCords, function(wd){
@@ -116,7 +152,7 @@ var icon = data.weather[0].icon;
 
 		$("#logo-img").attr("src","../images/logo2.svg");
 
-		$(".center-wrapp-footer").css("visibility","visible");
+		$(".center-wrapp-footer").css("visibility","visible"); 
 
 
 	},500);
@@ -125,76 +161,82 @@ var icon = data.weather[0].icon;
 });
 
 
-var apiForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&mode=xml&APPID=eaf69a1d8aef49e96abdc8a9b590ba64&units=metric";
+// var apiForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&mode=xml&APPID=eaf69a1d8aef49e96abdc8a9b590ba64&units=metric";
 
 
-$.ajax({
+// $.ajax({
 
-    url: apiForecast,
-    context: document.body
+//     url: apiForecast,
+//     context: document.body
 
-    }).done(function (data) {
+//     }).done(function (data) {
 
-    var today = data.documentElement.children[3].attributes[0].nodeValue;
+//     var today = data.documentElement.children[3].attributes[0].nodeValue;
    
-    var todaydate = new Date(today);
+//     var todaydate = new Date(today);
     
-    var todaytest = todaydate.getDay();
+//     var todaytest = todaydate.getDay();
    
-    var j = 0;
+//     var j = 0;
    
 
-    for (let i = 0; i < 5; i++) {        
+//     // for (let i = 0; i < 5; i++) {        
        
-        var date2 = new Date(data.firstChild.lastChild.children[j].attributes[0].nodeValue);
+//     //     var date2 = new Date(data.firstChild.lastChild.children[j].attributes[0].nodeValue);
        
-        var test = date2.getDay();
+//     //     var test = date2.getDay();
        
-        while (test == todaytest){
-            j++;
-            date2 = new Date(data.firstChild.lastChild.children[j].attributes[0].nodeValue);
-            test = date2.getDay();
-        }
+//     //     while (test == todaytest){
+//     //         j++;
+//     //         date2 = new Date(data.firstChild.lastChild.children[j].attributes[0].nodeValue);
+//     //         test = date2.getDay();
+//     //     }
 
-        $("#day" + i + "min").html(data.firstChild.lastChild.children[(i * 8)+j].children[4].attributes[2].nodeValue);
+//     //     $("#day" + i + "min").html(data.firstChild.lastChild.children[(i * 8)+j].children[4].attributes[2].nodeValue);
         
-        $("#day" + i + "max").html(data.firstChild.lastChild.children[(i * 8) + j].children[4].attributes[3].nodeValue);
+//     //     $("#day" + i + "max").html(data.firstChild.lastChild.children[(i * 8) + j].children[4].attributes[3].nodeValue);
 
-        var date = (data.firstChild.lastChild.children[(i * 8) + j].attributes[0].nodeValue).substring(0, 10);
+//     //     var date = (data.firstChild.lastChild.children[(i * 8) + j].attributes[0].nodeValue).substring(0, 10);
         
-        var finalDate = new Date(data.firstChild.lastChild.children[(i*8)+j].attributes[0].nodeValue);
+//     //     var finalDate = new Date(data.firstChild.lastChild.children[(i*8)+j].attributes[0].nodeValue);
         
-        var finalDay = finalDate.getDay();
+//     //     var finalDay = finalDate.getDay();
         
-        var weekDay = null;
-        switch(finalDay) {
-            case 0:
-                weekDay = "SUN";
-                break;
-            case 1:
-                weekDay = "MON";
-                break;
-            case 2:
-                weekDay = "TUE";
-                break;
-            case 3:
-                weekDay = "WED";
-                break;
-            case 4:
-                weekDay = "THU";
-                break;
-            case 5:
-                weekDay = "FRI";
-                break;
-            case 6:
-                weekDay = "SAT";
-                break;
-            default:
-                weekDay = "SUN";
-        }
-        $("#day" + i).html(weekDay);
-    }
-});
+//     //     var weekDay = null;
+//     //     switch(finalDay) {
+//     //         case 0:
+//     //             weekDay = "SUN";
+//     //             break;
+//     //         case 1:
+//     //             weekDay = "MON";
+//     //             break;
+//     //         case 2:
+//     //             weekDay = "TUE";
+//     //             break;
+//     //         case 3:
+//     //             weekDay = "WED";
+//     //             break;
+//     //         case 4:
+//     //             weekDay = "THU";
+//     //             break;
+//     //         case 5:
+//     //             weekDay = "FRI";
+//     //             break;
+//     //         case 6:
+//     //             weekDay = "SAT";
+//     //             break;
+//     //         default:
+//     //             weekDay = "SUN";
+//     //     }
+//     //     $("#day" + i).html(weekDay);
+//     // }
+// });
 	});
+
+
+
+
+
 };
+
 
